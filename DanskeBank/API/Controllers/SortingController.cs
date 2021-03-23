d8c33acdc6446840c764a0c339c2cfe8896b1822
Jakub Parteka <s16776@pjwstk.edu.pl>
2021-03-23 15:40:28 +0100

    empty repo init

---
+++ DanskeBank/API/Controllers/SortingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SortingController: ControllerBase
+    {
+        public SortingController()
+        {
+
+        }
+
+        [HttpPost]
+        public void BubbleSort([FromBody] double[] numbers)
+        {
+        }
+    }
+}
